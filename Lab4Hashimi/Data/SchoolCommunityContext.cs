5b99f0050a1e8e0169ce41719bbbc87b733cf069
hash0131 <hash0131@algonquinlive.com>
2021-06-26 02:53:51 -0400

    first commit

---
+++ Lab4Hashimi/Data/SchoolCommunityContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab4Hashimi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab4Hashimi.Data
+{
+    public class SchoolCommunityContext : DbContext
+    {
+        public SchoolCommunityContext(DbContextOptions<SchoolCommunityContext> options) : base(options)
+        {
+
+        }
+
+      public DbSet<Student> Students { get; set; }
+        public DbSet<CommunityMembership> CommunityMemberships { get; set; }
+        public DbSet<Community> Communities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Community>().ToTable("Community");
+            modelBuilder.Entity<CommunityMembership>().ToTable("CommunityMembership");
+
+            modelBuilder.Entity<CommunityMembership>()
+                .HasKey(c => new { c.StudentID, c.CommunityID });
+
+        }
+    }
+}
